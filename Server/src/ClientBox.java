8fad7731374677bd4d26c5a9f0c6f67163d94fbc
AyoubIMAMI <youbima17@gmail.com>
2022-09-28 16:02:16 +0200

    ClientBox class

---
+++ Server/src/ClientBox.java
@@ -0,0 +1,14 @@
+public class ClientBox {
+
+    ClientBox() {
+
+    }
+
+    /**
+     * Stream the movie to the client
+     * @param chunk data to send to the client
+     */
+    void stream(byte[] chunk) {
+
+    }
+}
