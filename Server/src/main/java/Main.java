4d6c373ebc002c401f409f2da67523f702814777
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-09-28 17:57:32 +0200

    Deleting legacy import

--- Server/src/main/java/Main.java
+++ Server/src/main/java/Main.java
@@ -5,10 +5,8 @@
  * VOD_RMI_project
  */
 
-import java.io.IOException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
 
 public class Main {
     public static void main(String[] args) {
