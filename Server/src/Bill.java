6548640361faaee52e20879bda5d77cc602bc88a
AyoubIMAMI <youbima17@gmail.com>
2022-09-28 15:57:43 +0200

    Client class

--- Server/src/Bill.java
+++ Server/src/Bill.java
@@ -2,7 +2,9 @@ import java.math.BigInteger;
 
 public class Bill {
 
+    //movie name
     private String movieName;
+    //movie price
     private BigInteger outrageousPrice;
 
     Bill() {
---
+++ Server/src/Client.java
@@ -0,0 +1,19 @@
+public class Client {
+    //mail of the client
+    private String mail;
+    //password of the client
+    private String password;
+
+    Client () {
+
+    }
+
+    /**
+     * Check if the password matches the mail
+     * @param password of the user
+     * @return true if the password matches the login (mail)
+     */
+    boolean checkCredential(String password) {
+        return true;
+    }
+}
