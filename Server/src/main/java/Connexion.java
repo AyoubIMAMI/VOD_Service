6ca48f40eddc01e6bdb6413acc311bef6c1d5586
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-09-28 19:10:46 +0200

    Adding login method

--- Server/src/main/java/Connexion.java
+++ Server/src/main/java/Connexion.java
@@ -2,6 +2,7 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Connexion extends UnicastRemoteObject implements IConnection {
     private List<Client> clients;
@@ -16,13 +17,13 @@ public class Connexion extends UnicastRemoteObject implements IConnection {
      *
      * @param mail - The mail associated to the new account
      * @param password - The password associated to the new account
-     * @return - true/false: if the new account is successfully added to the client list
+     * @return - boolean: if the new account is successfully added to the client list
      * @throws RemoteException - Problem occurring during the networking
      */
     @Override
     public boolean signIn(String mail, String password) throws RemoteException {
         // Check if a client already had this email
-        if (clients.stream().anyMatch(client -> client.getMail().equals(mail)))
+        if (clients.stream().anyMatch(client -> client.checkMail(mail)))
             return false;
 
         System.out.println("[INFO] New user '" + mail + "' added to database.");
@@ -30,9 +31,21 @@ public class Connexion extends UnicastRemoteObject implements IConnection {
         return true;
     }
 
+    /**
+     *
+     * @param mail - The mail associated to the new account
+     * @param password - The password associated to the new account
+     * @return - An IVODService if the client successfully logged in, null otherwise
+     * @throws RemoteException - Problem occurring during the networking
+     */
     @Override
     public IVODService login(String mail, String password) throws RemoteException {
         System.out.println("[INFO] " + mail + " is attempting to log in");
-        return vodService;
+
+        // Check if a client had the given email and the given password
+        if(clients.stream().filter(client -> client.checkMail(mail)).anyMatch(client -> client.checkPass(password)))
+            return vodService;
+
+        return null;
     }
 }
